6edc1ded555bef6bcfd222c9ad1eed18d065142d
Viktor Strelianyi <chmpain@gmail.com>
2019-01-22 20:00:14 +0200

    - bug fixes

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ Raindrop.prototype.render = function(ctx) {
   ctx.closePath();
   ctx.stroke();
 };
+
 Raindrop.prototype.update = function() {
   this.x += this.hVelocity;
   this.y += this.velocity;
@@ -30,10 +31,11 @@ Raindrop.prototype.update = function() {
   }
 };
 Raindrop.prototype.isOffscreen = function() {
-  return;
-  this.y > ScreenSize.HEIGHT + this.size ||
+  return (
+    this.y > ScreenSize.HEIGHT + this.size ||
     this.x > ScreenSize.WIDTH + this.size ||
-    this.x < -this.size;
+    this.x < -this.size
+  );
 };
 
 Raindrop.prototype._reset = function() {
@@ -44,6 +46,39 @@ Raindrop.prototype._reset = function() {
 
   this.velocity = this.size;
   this.hVelocity = -this.size / 3;
+};
+
+var Cucumber = function() {
+  Raindrop.call(this);
+};
+
+Cucumber.prototype = Object.create(Raindrop.prototype);
+
+Cucumber.prototype.render = function(ctx) {
+  ctx.fillStyle = "green";
+  ctx.beginPath();
+  ctx.ellipse(
+    this.x,
+    this.y,
+    this.size,
+    this.size * 3,
+    this.angle,
+    0,
+    Math.PI * 2,
+    false
+  );
+  ctx.closePath();
+  ctx.fill();
+};
+
+Cucumber.prototype.update = function() {
+  Raindrop.prototype.update.call(this);
+  this.angle += 0.01;
+};
+
+Cucumber.prototype._reset = function() {
+  Raindrop.prototype._reset.call(this);
+  this.angle = getRandomValue(0, Math.PI * 2);
 };
 
 // clean from previous objects
@@ -64,16 +99,23 @@ var renderFrame = function(ctx, raindrops) {
 
 var setup = function() {
   var DROPS = 600;
+  var CUCUMBER_RATIO = 0.2;
   var canvas = document.querySelector("#playground");
   var ctx = canvas.getContext("2d");
 
   canvas.width = ScreenSize.WIDTH;
   canvas.height = ScreenSize.HEIGHT;
 
-  var raindrops = new Array(DROPS).fill("").map(function() {
-    return new Raindrop();
-  });
-
+  var raindrops = new Array(DROPS * (1 - CUCUMBER_RATIO))
+    .fill("")
+    .map(function() {
+      return new Raindrop();
+    })
+    .concat(
+      new Array(DROPS * CUCUMBER_RATIO).fill("").map(function() {
+        return new Cucumber();
+      })
+    );
   renderFrame(ctx, raindrops);
 };
 
